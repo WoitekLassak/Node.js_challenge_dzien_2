1b7a4e9870055efb3ff3dd31f39f55c115fce0e9
WoitekLassak <97149981+WoitekLassak@users.noreply.github.com>
2022-01-08 19:19:38 +0100

    Update zadanie01.js

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
+var items = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+var temp1;
+var temp2;
+for (let i=0;i<=2;){
+  for(let j=0;j<=1;){
+   
+    console.log(items[i][j]);
+    j++;
+  }
+  
+    i++;
+}
 //Twój kod
